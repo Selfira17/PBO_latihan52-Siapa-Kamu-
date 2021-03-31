2490fa6e6f608183cab9b694610a4b081d3d77a5
selfira <selfiraayw@gmail.com>
2021-03-31 21:23:34 +0700

    PBO latihan52 siapa kamu

---
+++ src/pbo_latihan2/siapa/kamu/PBO_latihan2SiapaKamu.java
@@ -0,0 +1,41 @@
+/*Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+ * Deskripsi : Latihan 52"Siapa Kamu"
+ */
+package pbo_latihan2.siapa.kamu;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO_latihan2SiapaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String getNip ="41227829930";
+    String siapasaya ="saya Dosen";
+    
+    System.out.println("NIP Dosen   :"+getNip);
+    System.out.println(siapasaya);
+    System.out.println("Saya Rizki Adam Kurniawan umur 27 tahun sedang mengajar matakuliah PBO");
+    
+    
+    String getNim = "11010269";
+    String siapaaku = "saya Mahasiswa";
+    
+    
+    System.out.println("NIM Mahasiswa :"+getNim);
+    System.out.println(siapaaku);
+    System.out.println("Saya Nindi umur 17 tahun sedang belajar di kelas PBO2");
+    
+    
+    
+    
+    }
+    
+}
